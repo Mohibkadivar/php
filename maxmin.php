dec8a5cd5d49cd37120c6883de5c64e0897dec34
Mohibkadivar <146947858+Mohibkadivar@users.noreply.github.com>
2025-08-14 21:36:51 +0530

    Add files via upload

---
+++ maxmin.php
@@ -0,0 +1,11 @@
+<?php
+    $number = array(10,20,30);
+
+    $max_number=max($number);
+    $min_number=min($number);
+
+    echo "maximum : " .$max_number;
+    echo "<br>";
+    echo "minimum : " .$min_number;
+    
+?>
